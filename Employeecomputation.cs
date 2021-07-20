004e5d44a6f6421c6551bc81f7e1a65d9b077627
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-20 13:40:18 +0530

    [manish]added:Welcome message

---
+++ Employeecomputation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    class Employeecomputation
+    {
+        public void start()
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employeecomputation Employee = new Employeecomputation();
+            Employee.start();
+        }
+    }
+}
