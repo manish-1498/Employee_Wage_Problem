6ec619dcd1ba255ff54ce018913b4619c371cde1
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-20 21:52:11 +0530

    [manish] added:wages per month

--- EmployeeWageProblem/Employeecomputation.cs
+++ EmployeeWageProblem/Employeecomputation.cs
@@ -13,22 +13,25 @@ namespace EmployeeWageProblem
             int fulldayhour = 8;
             int parttimehour = 4;
             int dailywage = 0;
+            int workingdayspermonth = 20;
+            int wagespermonth = 0;
             Random random = new Random();
             int empcheck = random.Next(0, 2);
             switch (empcheck)
             {
                 case 0:
-                    Console.WriteLine("Employee is absent");
-                    break;
-                case 1:
                     Console.WriteLine("Employee is present");
                     dailywage = fulldayhour * wageperhour;
+                    wagespermonth = dailywage * workingdayspermonth;
                     Console.WriteLine("dailywage is"+dailywage);
+                    Console.WriteLine("wages per month is" + wagespermonth);
                     break;
-                case 2:
+                case 1:
                     Console.WriteLine("Employee is present and doing part time");
                     dailywage = parttimehour * wageperhour;
+                    wagespermonth = dailywage * workingdayspermonth;
                     Console.WriteLine("dailywage is" + dailywage);
+                    Console.WriteLine("wages per month is" + wagespermonth);
                     break;
             }
         }
